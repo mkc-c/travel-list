9bf3d331e19b5ebbb4a7e5d97e516418362bab65
mkc-c <abdirisak.arr@gmail.com>
2023-12-24 11:47:23 +0000

    hardcode placeholder select, input, submit button

--- src/App.js
+++ src/App.js
@@ -22,7 +22,14 @@ function Logo() {
 function Form() {
   return (
     <form className="add-form">
-      <h3>What do you need for your 😍 trip </h3>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </form>
   );
 }
