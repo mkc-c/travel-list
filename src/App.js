234dfa662c0d65b991f32ee3f3802f1a81c04e96
mkc-c <abdirisak.arr@gmail.com>
2023-12-24 11:21:36 +0000

    add seed data, map over seed data, create item component mapped to seed data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,8 +27,16 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
 }
+
+function Item({ item }) {}
 
 function Stats() {
   return (
