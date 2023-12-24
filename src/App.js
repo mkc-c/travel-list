e378503d40cf74248bd0fa71a28645142b15c9e2
mkc-c <abdirisak.arr@gmail.com>
2023-12-24 17:33:49 +0000

    make input a controlled element

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -20,6 +22,8 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -34,7 +38,12 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <button>Add</button>
     </form>
   );
