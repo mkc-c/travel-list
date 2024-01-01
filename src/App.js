56b9d5b9bdbc7bcc8cfb65f9b6462b515724df09
mkc-c <abdirisak.arr@gmail.com>
2024-01-01 07:26:38 +0000

    too many changes to track, self documenting changes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,22 +27,37 @@ function Logo() {
   return <h1>🌴 Far Away 👜</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setQuantity(1);
+    setDescription("");
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
-        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))}
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => {
+          return (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          );
+        })}
       </select>
       <input
         type="text"
@@ -49,22 +70,23 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
+        {items.map((item) => {
+          return <Item item={item} key={item.description} />;
+        })}
       </ul>
     </div>
   );
 }
 
 function Item({ item }) {
+  console.log(item);
   return (
     <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+      <span>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
