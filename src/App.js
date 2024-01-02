7e713487168249c05d746b808e700e85f9a8ffe5
mkc-c <abdirisak.arr@gmail.com>
2024-01-02 15:18:58 +0000

    add derived state to footer

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handletoggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -115,10 +115,17 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        👜 You have {numItems} items on your list, and you already packed
+        {numPacked} ({percentage}%)
+      </em>
     </footer>
   );
 }
